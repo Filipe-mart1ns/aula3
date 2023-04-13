6ae89b736a9ffd92c8bffda84165552d374c4c97
Filipe Martins <filipemartins03032004@gmial.com>
2023-04-13 12:30:00 -0300

    correção da aula do dia 13-04

--- src/controllers.js
+++ src/controllers.js
@@ -1,55 +1,50 @@
+
+
 const products = [
-{
-    id: 1,
-    descricao: 'coca-cola 2lts',
-    valor: 10.00,
-    qtde: 10
-},
-{
-    id: 2,
-    descricao: 'cereja',
-    valor: 5.80,
-    qtde: 50
-},
-{
-    id: 3,
-    descricao: 'agua',
-    valor: 2.00,
-    qtde: 5
-},
-
-]
-
-
-module.exports = {
-    async creatcart(request, response) {
-
-        const{ id, descricao, valor, qtde } = request.body
-
-        const existesproduct = products.find(product => product.id === id)
-        console.log(existesproduct)
-        if (existesproduct) {
-            return response.status(400).json({
-                Error: 'protudo ja cadastrado'
-
-            })
-        }
-
-        request.body.id
-        request.body.descricao
-
-        products.push({
-            id,
-            descricao,
-            valor,
-            qtde
-        })
+    {
+      id: 1,
+      descricao: 'Coca cola 2lts',
+      valor: 10.00,
+      qtde: 10
     },
-    async createcart(request, response) {
-        return response.json({data: products })
-     },
-
-    async hello2(request, response) {
-        return response.json({msg: 'hello world 2'})
-     }
-}
+    {
+      id: 2,
+      descricao: "Cerveja",
+      valor: 5.80,
+      qtde: 50
+    },
+    {
+      id: 3,
+      descricao: 'Agua',
+      valor: 2.00,
+      qtde: 5
+    }
+  ]
+  
+  module.exports = {
+    async createCart(request, response) {
+  
+      const { id, descricao, valor, qtde } = request.body
+  
+      const existsProduct = products.find(product => product.id === id)
+  
+      console.log(existsProduct)
+      if (existsProduct) {
+        return response.status(400).json({
+          error: 'Produto já cadastrado'
+        })
+      }
+  
+      //Adicionar um novo elemento no array, usamos o push
+      products.push({
+        id,
+        descricao,
+        valor,
+        qtde
+      })
+  
+  
+      return response.json({ data: products })
+    },
+  }
+  
